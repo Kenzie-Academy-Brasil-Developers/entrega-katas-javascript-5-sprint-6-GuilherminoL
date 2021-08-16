b461d2e1e552e0f37ad9cdaa9dccdf2bb81fbdef
Guilhermino Lucas <guilherminolucasc@gmail.com>
2021-08-16 16:49:15 -0300

    Adding the first and the second function

--- katas5.js
+++ katas5.js
@@ -1,1 +1,38 @@
 // implemente aqui as funções de teste
+function testeReverseString1() {
+    let result = reverseString("Kenzie Academy");
+    let expected = "ymedacA eizneK";
+    console.assert(result === expected, `esperado: ${expected}, obtido: ${result}`)
+ }
+ function testeReverseString2() {
+    let result = reverseString("1 2 3");
+    let expected = "3 2 1";
+    console.assert(result === expected, `esperado: ${expected}, obtido: ${result}`)
+ }
+
+ const reverseString = (string) =>{
+    let output = string.split('').reverse().join('')
+    return output
+ }
+
+ testeReverseString1()
+ testeReverseString2()
+
+ function testReverseSentence1(){
+     let result = reverseSentence('Eu sou Guilhermino')
+     let expected = ('Guilhermino sou Eu')
+     console.assert(result === expected, `esperado ${expected}, obtido: ${result}`)
+ }
+
+ function testReverseSentence2(){
+    let result = reverseSentence('I <3 Kenzie')
+    let expected = ('Kenzie <3 I')
+    console.assert(result === expected, `esperado ${expected}, obtido: ${result}`)
+}
+
+const reverseSentence = (str) =>{
+    let output = str.split(' ').reverse().join(' ')
+    return output
+}
+testReverseSentence1()
+testReverseSentence2()
